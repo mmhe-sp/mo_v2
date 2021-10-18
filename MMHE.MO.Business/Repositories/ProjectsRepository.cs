93b32694b9b64f0aa963657b5b1adadbbf78623a
Sachin Pawar <sachin@simbiotiktech.com>
2021-10-19 03:51:06 +0800

    Updated Project Search

--- MMHE.MO.Business/Repositories/ProjectsRepository.cs
+++ MMHE.MO.Business/Repositories/ProjectsRepository.cs
@@ -1,5 +1,5 @@
-﻿using MMHE.MO.Models;
-
+﻿using MMHE.MO.Business.Entities;
+using MMHE.MO.Models;
 using System;
 using System.Collections.Generic;
 using System.Data;
@@ -10,28 +10,54 @@ using System.Threading.Tasks;
 
 namespace MMHE.MO.Business.Repositories
 {
-	public class ProjectsRepository
-	{
-		public List<Option> FindProjects(string project)
-		{
-			SqlParameter[] parameters = new SqlParameter[3];
-			parameters[0] = new SqlParameter("@project", project);
-			DataTable table = new DataTable();
-			using (SqlConnection connection = new SqlConnection(ConnectionStringHelper.MO))
-			{
-				using (SqlCommand command = new SqlCommand("MO.FindProjects", connection))
-				{
-					command.CommandType = CommandType.StoredProcedure;
-					command.Parameters.AddRange(parameters);
-					SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
-					sqlDataAdapter.Fill(table);
-				}
-			}
-			return table.Rows.Cast<DataRow>().Select(d => new Option
-			{
-				Value = d.Field<string>("ProjectId"),
-				Text = d.Field<string>("ProjectName")
-			}).ToList();
-		}
-	}
+    public class ProjectsRepository
+    {
+        public List<Option> FindProjects(string project)
+        {
+            SqlParameter[] parameters = new SqlParameter[1];
+            parameters[0] = new SqlParameter("@Project", project);
+            DataTable table = new DataTable();
+            using (SqlConnection connection = new SqlConnection(ConnectionStringHelper.MO))
+            {
+                using (SqlCommand command = new SqlCommand("MO.FindProjects", connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+                    command.Parameters.AddRange(parameters);
+                    SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                    sqlDataAdapter.Fill(table);
+                }
+            }
+            return table.Rows.Cast<DataRow>().Select(d => new Option
+            {
+                Value = d.Field<string>("ProjectId"),
+                Text = d.Field<string>("ProjectName")
+            }).ToList();
+        }
+
+        public void UpdateUserProject(string projectId, string userId)
+        {
+            using (MOContext context = new MOContext())
+            {
+                //var user = context.tblUsers.Single(d => d.EmployeeName == userName);
+                int id = int.Parse(userId);
+                var session = context.tblUserSessionProjectInfoes.Where(d => d.EmployeeId == id).OrderByDescending(d => d.ModifiedDate).FirstOrDefault();
+                if (session == null)
+                {
+                    context.tblUserSessionProjectInfoes.Add(new tblUserSessionProjectInfo
+                    {
+                        CreatedDate = DateTime.Now,
+                        EmployeeId = id,
+                        ModifiedDate = DateTime.Now,
+                        ProjectID = projectId
+                    });
+                }
+                else
+                {
+                    session.ProjectID = projectId;
+                    session.ModifiedDate = DateTime.Now;
+                }
+                context.SaveChanges();
+            }
+        }
+    }
 }
--- MMHE.MO/Services/Project.asmx.cs
+++ MMHE.MO/Services/Project.asmx.cs
@@ -7,14 +7,23 @@ using System.Web.Services;
 
 namespace MMHE.MO.Services
 {
-	public class Project : BaseWebService
-	{
-		[WebMethod]
-		[ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
-		public void FindProjects(string project)
-		{
-			JavaScriptSerializer js = new JavaScriptSerializer();
-			Context.Response.Write(js.Serialize(new ProjectsRepository().FindProjects(project).Select(p => p.Value + "-" + p.Text)));
-		}
-	}
+    public class Project : BaseWebService
+    {
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void FindProjects(string project)
+        {
+            JavaScriptSerializer js = new JavaScriptSerializer();
+            Context.Response.Write(js.Serialize(new ProjectsRepository().FindProjects(project).Select(p => p.Value + "-" + p.Text)));
+        }
+
+        [WebMethod]
+        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+        public void UpdateProject(string project)
+        {
+            string projectNumber = project.Split('-')[0];
+            new ProjectsRepository().UpdateUserProject(projectNumber, LoggedInUser.Id);
+            Context.Response.Write(new JavaScriptSerializer().Serialize(new { Ok = true }));
+        }
+    }
 }
