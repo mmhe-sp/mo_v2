a0978c1cc55d3809986786eb463011491a26bc79
Sachin Pawar <sachin.pawar@nobrainsolutions.com>
2021-10-18 17:52:24 +0530

    Added find projects functionality

---
+++ MMHE.MO.Business/Repositories/ProjectsRepository.cs
@@ -0,0 +1,37 @@
+﻿using MMHE.MO.Models;
+
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MMHE.MO.Business.Repositories
+{
+	public class ProjectsRepository
+	{
+		public List<Option> FindProjects(string project)
+		{
+			SqlParameter[] parameters = new SqlParameter[3];
+			parameters[0] = new SqlParameter("@project", project);
+			DataTable table = new DataTable();
+			using (SqlConnection connection = new SqlConnection(ConnectionStringHelper.MO))
+			{
+				using (SqlCommand command = new SqlCommand("MO.FindProjects", connection))
+				{
+					command.CommandType = CommandType.StoredProcedure;
+					command.Parameters.AddRange(parameters);
+					SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+					sqlDataAdapter.Fill(table);
+				}
+			}
+			return table.Rows.Cast<DataRow>().Select(d => new Option
+			{
+				Value = d.Field<string>("ProjectId"),
+				Text = d.Field<string>("ProjectName")
+			}).ToList();
+		}
+	}
+}
--- MMHE.MO/Services/Project.asmx.cs
+++ MMHE.MO/Services/Project.asmx.cs
@@ -1,23 +1,20 @@
-﻿using MMHE.MO.Models;
-using System.Collections.Generic;
+﻿using MMHE.MO.Business.Repositories;
+
+using System.Linq;
 using System.Web.Script.Serialization;
 using System.Web.Script.Services;
 using System.Web.Services;
 
 namespace MMHE.MO.Services
 {
-    public class Project : BaseWebService
-    {
-        [WebMethod]
-        [ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
-        public void FindProjects(string project)
-        {
-            var serializer = new JavaScriptSerializer();
-
-
-            JavaScriptSerializer js = new JavaScriptSerializer();
-            var projects = new List<string> { "1234-Project1", "1234-Project1", "1234-Project1", "1234-Project1", "1234-Project1" };
-            Context.Response.Write(js.Serialize(projects));
-        }
-    }
+	public class Project : BaseWebService
+	{
+		[WebMethod]
+		[ScriptMethod(ResponseFormat = ResponseFormat.Json, UseHttpGet = true)]
+		public void FindProjects(string project)
+		{
+			JavaScriptSerializer js = new JavaScriptSerializer();
+			Context.Response.Write(js.Serialize(new ProjectsRepository().FindProjects(project).Select(p => p.Value + "-" + p.Text)));
+		}
+	}
 }
