9d7ea6abddf89f68fee531b4aa48e5e1afc6e6a7
Sachin Pawar <sachin.pawar@nobrainsolutions.com>
2021-11-16 10:10:17 +0530

    Updated code to create new VO

--- MMHE.MO/ControlTemplates/MMHE.MO/VO/Manage.ascx.cs
+++ MMHE.MO/ControlTemplates/MMHE.MO/VO/Manage.ascx.cs
@@ -1,50 +1,58 @@
 ﻿using MMHE.MO.Business.Repositories;
 using MMHE.MO.Models;
 using MMHE.MO.UI;
+
 using System;
 using System.Web.UI;
 
 namespace MMHE.MO.Controls.VO
 {
-    public partial class Manage : UserControl
-    {
-        public VODetails Details { get; set; }
-        public bool CanApprove { get; set; }
-        public bool CanPrint { get; set; }
-        public int LastRowIndex = 0;
-        public string JCSId = string.Empty;
-        protected void Page_Load(object sender, EventArgs e)
-        {
-            var user = (Page as BasePage).LoggedInUser;
-            bool isNew = false;
-            JCSId = Request.QueryString["id"];
-            if (string.IsNullOrWhiteSpace(JCSId))
-            {
-                isNew = true;
-                JCSId = Guid.Empty.ToString();
-            }
-            Details = new VORepository().GetVODetails(JCSId, user.ProjectId, user.Id);
-            Details.Type = Type =="A"? "Additional Work Order": "Variation Order";
-            jcsRepeater.DataSource = Details.Activities;
-            jcsRepeater.DataBind();
-            LastRowIndex = Details.Activities.Count + 1;
-            if (Details.CanApprove && !isNew)
-                CanApprove = true;
+	public partial class Manage : UserControl
+	{
+		public VODetails Details { get; set; }
+		public bool CanApprove { get; set; }
+		public bool CanPrint { get; set; }
+		public bool CanChange { get; set; }
+		public int LastRowIndex = 0;
+		public string JCSId = string.Empty;
+		public string voId = null;
+		protected void Page_Load(object sender, EventArgs e)
+		{
+			var user = (Page as BasePage).LoggedInUser;
+			bool isNew = false;
+			JCSId = Request.QueryString["id"];
+			voId = Request.QueryString["jcs"];
+			if (string.IsNullOrWhiteSpace(JCSId))
+			{
+				isNew = true;
+				JCSId = Guid.Empty.ToString();
+			}
 
-            if (Details.CanPrint && !isNew)
-                CanPrint = true;
-        }
+			Details = new VORepository().GetVODetails(JCSId, voId, user.ProjectId, user.Id);
+			Details.Type = Type == "A" ? "Additional Work Order" : "Variation Order";
+			jcsRepeater.DataSource = Details.Activities;
+			jcsRepeater.DataBind();
+			LastRowIndex = Details.Activities.Count + 1;
+			if (Details.CanApprove && !isNew)
+				CanApprove = true;
 
-        public string Type
-        {
-            get
-            {
-                string type = Request.QueryString["type"];
-                if (string.IsNullOrWhiteSpace(type) || string.Equals(type, "v", StringComparison.InvariantCultureIgnoreCase))
-                    return "V";
-                else
-                    return "A";
-            }
-        }
-    }
+			if (Details.CanPrint && !isNew)
+				CanPrint = true;
+			if (Details.CanPrint && !isNew)
+				CanPrint = true;
+			CanChange = !string.IsNullOrWhiteSpace(voId);
+		}
+
+		public string Type
+		{
+			get
+			{
+				string type = Request.QueryString["type"];
+				if (string.IsNullOrWhiteSpace(type) || string.Equals(type, "v", StringComparison.InvariantCultureIgnoreCase))
+					return "V";
+				else
+					return "A";
+			}
+		}
+	}
 }
