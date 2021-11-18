42189baaf023a94551aecd3e7cbbe7406d51bede
Sachin Pawar <sachin.pawar@nobrainsolutions.com>
2021-11-18 07:50:21 +0530

    Updated code for DWC UI

--- MMHE.MO/ControlTemplates/MMHE.MO/DWC.ascx.cs
+++ MMHE.MO/ControlTemplates/MMHE.MO/DWC.ascx.cs
@@ -7,8 +7,12 @@ namespace MMHE.MO.Controls
 {
 	public partial class DWC : UserControl
 	{
+		public string Today { get; set; }
+		public string Tomorrow { get; set; }
 		protected void Page_Load(object sender, EventArgs e)
 		{
+			Today = DateTime.Now.ToString("dd-MMM-yy");
+			Tomorrow = DateTime.Now.AddDays(1).ToString("dd-MMM-yy");
 		}
 	}
 }
