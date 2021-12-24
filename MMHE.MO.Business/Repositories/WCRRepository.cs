15acdd418c9df1483406fd996ee227f7efffe65a
Ext Simbiotik 03 <ext.simbiotik03@mmhe.com.my>
2021-12-24 15:37:12 +0800

    Added WCR repo

--- MMHE.MO.Business/Repositories/WCRRepository.cs
+++ MMHE.MO.Business/Repositories/WCRRepository.cs
@@ -19,12 +19,73 @@ namespace MMHE.MO.Business.Repositories
                 {
                     command.CommandType = CommandType.StoredProcedure;
                     command.Parameters.AddWithValue("@Employeeid", loggedInUser);
-                    //command.Parameters.AddRange(parameters);
                     SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
                     sqlDataAdapter.Fill(table);
                 }
             }
             return table;
         }
+
+        public bool UpdateWCRStatus(string loggedInUser, string ownerNo)
+        {            
+            try
+            {
+                SqlParameter[] parameters = new SqlParameter[2];
+                parameters[0] = new SqlParameter("@Employeeid", loggedInUser);
+                parameters[1] = new SqlParameter("@OwnerNo", ownerNo);
+                using (SqlConnection connection = new SqlConnection(ConnectionStringHelper.MO))
+                {
+                    using (SqlCommand command = new SqlCommand("MO.UpdateWCRStatus", connection))
+                    {
+                        command.CommandType = CommandType.StoredProcedure;
+                        command.Parameters.AddRange(parameters);
+                        connection.Open();
+                        command.ExecuteNonQuery();
+                        connection.Close();
+                    }
+                }
+                return true;
+            }
+            catch
+            {
+                return false;
+            } 
+        }
+
+        //public bool CheckWCRStatus(string loggedInUser, string ownerNo)
+        //{
+        //    string status = "";
+        //    try
+        //    {               
+        //        SqlParameter[] parameters = new SqlParameter[2];
+        //        parameters[0] = new SqlParameter("@Employeeid", loggedInUser);
+        //        parameters[1] = new SqlParameter("@OwnerNo", ownerNo);
+        //        using (SqlConnection connection = new SqlConnection(ConnectionStringHelper.MO))
+        //        {
+        //            using (SqlCommand command = new SqlCommand("MO.CheckWCRStatus", connection))
+        //            {
+        //                command.CommandType = CommandType.StoredProcedure;
+        //                command.Parameters.AddRange(parameters);
+        //                connection.Open();
+        //                command.ExecuteNonQuery();
+        //                status = (string)command.Parameters["@DtWCRAct"].Value;
+        //                connection.Close();
+        //            }
+        //        }
+        //        if (status == "1")
+        //        {
+        //            return false;
+        //        }
+        //        else
+        //        {
+        //            return true;
+        //        }
+                
+        //    }
+        //    catch(Exception ex)
+        //    {
+        //        return false;
+        //    }
+        //}
     }
 }
